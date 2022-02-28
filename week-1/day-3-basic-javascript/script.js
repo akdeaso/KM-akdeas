d4827e7fe3674e249c39b2f0160e2d7661cde55e
Akdeas Oktanae <akdeaso@gmail.com>
2022-02-28 20:12:52 +0700

    belajar js, basic algorithm, function, looping, conditional, comparison

---
+++ week-1/day-3-basic-javascript/script.js
@@ -0,0 +1,12 @@
+console.log("day 3")
+const harga = 22222.22
+console.log(typeof harga)
+
+const firstname = "aku"
+const lastname = "saya"
+
+console.log(firstname + lastname)
+console.log(5 >= 6)
+
+const isTrue = (20 > 80) === true
+console.log(isTrue)
---
+++ week-1/day-5-looping-function/script.js
@@ -0,0 +1,37 @@
+//looping
+
+//for
+for (let i = 0; i < 10; i = i + 3) {
+    console.log("ea");
+}
+
+//while
+let idx = 1;
+while (idx < 10) {
+    console.log("ini");
+    console.log("while");
+
+    idx = idx + 3;
+    idx++;
+}
+
+//do while
+do {
+    console.log('brmmmmmmmmmmmmm');
+
+    idx++;
+} while (idx < 10);
+
+//function
+//biasa
+function malam(nama) {
+    console.log("selamat malam " + nama)
+}
+malam("gan");
+
+//expression
+const jumlah = function (x, y) {
+    console.log(x + y);
+}
+jumlah(1, 69)
+
