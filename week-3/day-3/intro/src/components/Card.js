55476b3523ddf0efda25ceeae9809218ea503c23
akdeaso <akdeaso@gmail.com>
2022-03-05 15:22:26 +0700

    log 04/03/2022

---
+++ week-3/day-3/intro/src/components/Card.js
@@ -0,0 +1,12 @@
+import react from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+const Card = () => {
+    return (
+        <View>
+            <Text>Ini adalah card</Text>
+        </View>
+    )
+}
+
+export default card
---
+++ week-3/day-3/intro/src/components/OptionList.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function OptionList() {
+    return (
+        <View style={{ backgroundColor: 'blue', }}>
+            <Text>Mobil</Text>
+        </View>
+    )
+}
